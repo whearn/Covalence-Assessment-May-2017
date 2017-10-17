8f1702f155fc413c789a2c0d037ff569221ffb28
whearn <wchearn00@gmail.com>
2017-10-17 16:49:22 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function learnMoreAlert() {
+    alert("TLAP is the result of a 10 year collaborative study between Dr. Andrews and Dr. Wilk. By studying the motions and records of over 50 MLB pitchers, Andrews and Wilk have determined the healthiest pitching method for pitchers both young and old alike. TLAP seeks to reduce the number of pitching related baseball injuries, as well as reducing the number of Tommy John Surgeries performed each year.");
+}
+
+function cbsAndrewsAlert() {
+    alert("This video is unavailable right now. Please try again later.");
+}
+
+function jerseyWatchAlert() {
+    alert("Search for TLAP on the app store, and download today!");
+}
+
+function surgeryAlert() {
+    alert("Improve your pitch, and play a healthier game with TLAP today. Educate your team members, and help reduce the number of Tommy John Surgeries performed each year.");
+}
